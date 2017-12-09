7eb908509bab75dd7b412b9a85ccda1f194758b7
Christina Källkrans <christinakallkrans@Christinas-MacBook-Pro.local>
2017-12-09 12:39:59 +0100

    Senaste versionen

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- Footer Begins -->
+	<table class="fotmeny">
+		<thead>
+		<tr>
+		  <th class="noUnderline"><a href="http://utbweb.its.ltu.se/~chrkll-4/om.php">Om</a></th>
+		  <th class="noUnderline"><a href="http://utbweb.its.ltu.se/~chrkll-4/om.php">Vanliga Frågor</a></th>
+		  <th class="noUnderline"><a href="http://utbweb.its.ltu.se/~chrkll-4/om.php">Användarvillkor</a></th>
+		</tr>
+		</thead>
+
+	</table>
